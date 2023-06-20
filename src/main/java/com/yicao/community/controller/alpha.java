c98889702668c1af7096b1d4cfd082d316b2be7d
yicao <yicaozyq@163.com>
2023-06-20 21:28:33 +0800

    0.01 项目搭建成功

--- src/main/java/com/yicao/community/controller/alpha.java
+++ src/main/java/com/yicao/community/controller/alpha.java
@@ -1,4 +1,16 @@
 package com.yicao.community.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/alpha")
 public class alpha {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "hello Spring Boot.";
+    }
 }
